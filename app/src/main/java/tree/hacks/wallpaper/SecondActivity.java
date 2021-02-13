afa15209cc29c31bd9d3fd7bb0c7189c54e9299b
jchen <jmschen888@gmail.com>
2021-02-13 12:21:40 -0800

    worked out second screen and group creation

--- app/src/main/java/tree/hacks/wallpaper/SecondActivity.java
+++ app/src/main/java/tree/hacks/wallpaper/SecondActivity.java
@@ -1,6 +1,10 @@
 package tree.hacks.wallpaper;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,6 +24,52 @@ public class SecondActivity extends AppCompatActivity {
 
         TextView wallpaperGroupNumber = (TextView) findViewById(R.id.wallpaperGroupNumber);
         text = getIntent().getExtras().getString("groupNum");
-        userName.setText("you are in group " + text);
+        wallpaperGroupNumber.setText("you are in group " + text);
+
+
+        Button leaveGroup = (Button) findViewById(R.id.leaveGroup);
+        Button confirmLeave = (Button) findViewById(R.id.confirmLeave);
+        Button cancelLeave = (Button) findViewById(R.id.cancelLeave);
+
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        int height = displayMetrics.heightPixels;
+        int width = displayMetrics.widthPixels;
+
+        leaveGroup.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                confirmLeave.setVisibility(View.VISIBLE);
+                cancelLeave.setVisibility(View.VISIBLE);
+            }
+        });
+
+        confirmLeave.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // leave the group in the database
+                Intent returnBtn = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(returnBtn);
+            }
+        });
+
+        cancelLeave.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                confirmLeave.setVisibility(View.INVISIBLE);
+                cancelLeave.setVisibility(View.INVISIBLE);
+            }
+        });
+
+    }
+
+    @Override
+    // make it so you can't accidentally leave the group by just pressing back
+    public void onBackPressed() {
+        return;
+    }
+
+    public void returnToMain() {
+        Intent setIntent = new Intent(Intent.ACTION_MAIN);
+        setIntent.addCategory(Intent.CATEGORY_HOME);
+        setIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(setIntent);
     }
 }
